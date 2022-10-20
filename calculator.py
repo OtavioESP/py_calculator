631e6a4e5fd98dfeb745ff5d448abe9d33a30f67
OtavioESP <otavio_emilio@outlook.com>
2022-10-19 23:01:12 -0300

    Feat: operations and deletion working

--- calculator.py
+++ calculator.py
@@ -5,12 +5,6 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 
 from components.button import Button
 
-positions = {
-	'left': {'x': 0, 'y': 400},
- 	'center':{'x': 0, 'y': 400},
-  	'right':{'x': 0, 'y': 400},
-    'operation_side':{'x': 0, 'y': 400}
-   }
 
 NUMERIC_BUTTONS = [
 	{'text': '1', 'width': 125, 'height': 50, 'x': 0, 'y': 400},
@@ -31,56 +25,75 @@ FUNCTIONAL_BUTTONS = [
 	{'text': '/', 'width': 125, 'height': 50, 'x': 375, 'y': 500},
 	{'text': '*', 'width': 125, 'height': 50, 'x': 375, 'y': 550},
 	{'text': '=', 'width': 125, 'height': 50, 'x': 250, 'y': 550},
-	# {'text': '.', 'width': 125, 'height': 50, 'x': 0, 'y': 550},
+	{'text': '.', 'width': 125, 'height': 50, 'x': 0, 'y': 550},
+	{'text': 'C', 'width': 250, 'height': 50, 'x': 0, 'y': 350},
+	{'text': 'CC', 'width': 250, 'height': 50, 'x': 250, 'y': 350},
 ]
+
+OPERATORS = ['+ ', '-', '/', '*']
 
 class Calculator(QMainWindow):
 	def __init__(self):
 		super().__init__()
-		self.first_number = 0
-		self.second_number = 0
-		self.current_number = ''
+		self.value = ''
+		self.historic = []
 		self.setWindowTitle('Calculadora')
 		[
-			self.render_numeric_buttons(
+			self._render_buttons(
 			btn['text'],
 			btn['width'],
 			btn['height'],
 			btn['x'],
 			btn['y']) for btn in NUMERIC_BUTTONS
 		]
-		# [
-		# 	self.render_functional_buttons(btn['text'] ,
-		# 	btn['width'],
-		# 	btn['height'],
-		# 	btn['x'],
-		# 	btn['y']) for btn in FUNCTIONAL_BUTTONS
-		# ]
-		self.main_label = self.render_label('0', 50, 50)
-		self.second_label = self.render_label('', 20, 50)
+		[
+			self._render_buttons(
+			btn['text'],
+			btn['width'],
+			btn['height'],
+			btn['x'],
+			btn['y']) for btn in FUNCTIONAL_BUTTONS
+		]
+		self.main_label = self._render_label('0', 50, 50)
 		self.setFixedWidth(500)
 		self.setFixedHeight(800)
 
+	def add_value(self, text):
+		if text == 'C':
+			self.value[:-1]
+			self.update_label(self.main_label, self.value)
+			return
+		
+		elif text == 'CC':
+			self.value = ''
+			self.update_label(self.main_label, self.value)
+			return
 
-	def button_clicked(self, text):
-		if text.isnumeric():
-			self.current_number += text
-			self.main_label.setText(self.current_number)
-			self.update(self.main_label)
 		elif text == '=':
-			self.first_number = self.current_number
-			
+			self.value = str(eval(self.value))
+			self.historic.append(self.value)
+			self.update_label(self.main_label, self.value)
+			return
 
+		elif text.isnumeric():
+			self.value += text
+			self.update_label(self.main_label, self.value)
+			return
 
-	def update(self, component):
-		component.adjustSize()
+		else:
+			self.value += f' {text} '
+			self.update_label(self.main_label, self.value)
+			return
 
-	def render_label(self, number, x, y):
+	def update_label(self, component, value):
+		component.setText(str(value))
+
+	def _render_label(self, number, x, y):
 		label = QLabel(f'{number}', self)
 		label.move(x, y)
 		return label
 
-	def render_numeric_buttons(self, text, width, height, x, y):
+	def _render_buttons(self, text, width, height, x, y):
 		button = QPushButton(text, self)
 		button.setStyleSheet('QPushButton {background-color: #A3C1DA; color: red;}')
 		# button.setToolTip('This is an example button')
@@ -88,17 +101,7 @@ class Calculator(QMainWindow):
 		button.setFixedWidth(width)
 		button.setFixedHeight(height)
 		button.move(x, y)
-		button.clicked.connect(lambda: self.button_clicked(text))
-
-	def render_functional_buttons(self, text, width, height, x, y):
-		button = QPushButton(text, self)
-		button.setStyleSheet('QPushButton {background-color: #A3C1DA; color: red;}')
-		# button.setToolTip('This is an example button')
-		# setToolTip should only be returned if a flag its true
-		button.setFixedWidth(width)
-		button.setFixedHeight(height)
-		button.move(x, y)
-		button.clicked.connect(lambda: self.button_clicked(text))
+		button.clicked.connect(lambda: self.add_value(text))
 
 
 app = QApplication(sys.argv)
